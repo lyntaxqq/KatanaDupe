628bc04555a89742395f347bbf1acf101f9f7678
Katana <232924959+Tealaxdev@users.noreply.github.com>
2026-01-09 18:40:17 +0300

    Refactor ItemFrameDrop class and imports

--- src/main/java/fr/Mrkatana/katanadupe/commands/ItemFrameDrop.java
+++ src/main/java/fr/Mrkatana/katanadupe/commands/ItemFrameDrop.java
@@ -2,13 +2,12 @@ package fr.Mrkatana.katanadupe.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
+import net.minecraft.command.CommandSource;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.item.Items;
-import net.minecraft.server.command.ServerCommandSource;
 import java.util.List;
 
-
-import net.minecraft.command.CommandSource;public class ItemFrameDrop extends Command {
+public class ItemFrameDrop extends Command {
     public ItemFrameDrop() {
         super("itemframedrop", "Çevrendeki item frame'lerdeki tüm eşyaları düşürür.");
     }
